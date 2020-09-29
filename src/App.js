024dfdeedb04a0ad030d71d29e8602e01bdd97e2
Boytobeaman <luxingchaode@gmail.com>
2020-09-29 18:15:36 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+import "./app.scss";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Code</h1>
+      <div className="a">
+        <div className="b">bbdbbb</div>
+      </div>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
